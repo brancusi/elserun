501dc4a3fe25c107b899aac4ce397fea4abd13de
Aram Zadikian <az@whatisfood.com>
2020-08-23 18:16:07 -0700

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-176144956-1`,
       },
     },
     `gatsby-plugin-feed`,
